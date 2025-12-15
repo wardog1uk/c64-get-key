6a2057bddb211c47b1a38b94124e4e81b3433217
Jonathan Mathews <wardog1uk@users.noreply.github.com>
2025-12-15 09:23:16 +0000

    Get input from buffer

--- src/get_key.asm
+++ src/get_key.asm
@@ -1,6 +1,21 @@
 *=* "get_key.asm"
 
+// KEYD - Start of keyboard buffer
+.const BUFFER = $0277
+
+// NDX - Number of characters in the keyboard buffer
+.const BUFFER_SIZE = $c6
+
+// ==========================================
+// Wait for keyboard input and store it in A
 // ==========================================
 get_key:
+    // wait for input
+    lda BUFFER_SIZE
+    beq get_key
+
+    // get first character from the buffer
+    lda BUFFER
+
     rts
 // ==========================================
