9b1efe4eb130a5f6219b798e0ffc2e295050344d
Jonathan Mathews <wardog1uk@users.noreply.github.com>
2025-12-17 18:02:44 +0000

    Improve comments

--- src/main.asm
+++ src/main.asm
@@ -1,24 +1,30 @@
 // ==========================================
 // C64 Key Input Test by Jonathan Mathews
 // ==========================================
+.const RETURN_KEY = $0d
+
+// ==========================================
 BasicUpstart2(main)
 jmp main
+// ==========================================
 
 #import "get_key.asm"
 
-.const RETURN_KEY = $0d
-
-
-*=* "Main"
 // ==========================================
+*=* "Main"
 main:
     // enable cursor blink
     lda #0
     sta $cc
 
     jsr get_key
+
+    // output character to screen
     jsr $ffd2
+
+    // loop until return key is pressed
     cmp #RETURN_KEY
     bne main
+    
     rts
 // ==========================================
